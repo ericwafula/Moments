ada0d2062ffd4dc06211a1c572848189160f5e77
Eric Wafula <wathomeeric@gmail.com>
2022-07-07 20:03:59 +0300

    Initial commit

---
+++ app/src/main/java/tech/ericwathome/moments/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package tech.ericwathome.moments.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import tech.ericwathome.moments.R
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/tech/ericwathome/moments/util/MomentumApplication.kt
@@ -0,0 +1,8 @@
+package tech.ericwathome.moments.util
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MomentumApplication : Application() {
+}
